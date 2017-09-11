0bdf72d098c51700d72ec09bbe1a2ae64a2598b3
AndreSaxton <andre_saxton@hotmail.com>
2017-09-11 16:24:17 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,5 +1,16 @@
 <?php
-$conexao = new mysqli('localhost', 'root','', 'bdtrabalho'); 
+
+try {
+    $conn = new PDO("sqlsrv:server = tcp:serverpresentacao1309.database.windows.net,1433; Database = bdapresentacao", "saxton", "apresentac@0");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+$conexao = $conn;
+
+//$conexao = new mysqli('localhost', 'root','', 'bdtrabalho'); 
 //Variavel = new mysqli("servidor", "usuario", "senha", "banco") or die("Erro ao conectar");
 	if (!$conexao)
 	{ 
