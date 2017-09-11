765ee73ec181a169fd11f67f662d11f386cafcbd
AndreSaxton <andre_saxton@hotmail.com>
2017-09-11 16:10:42 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,13 +1,5 @@
 <?php
-
-$host = 'serverpresentacao1309.database.windows.net';
-$username = 'saxton';
-$password = 'apresentac@0';
-$db_name = 'bdapresentacao1309';
-$conn = mysqli_init();
-$conexao($host, $username, $password, $db_name, 3306);
-
-//$conexao = new mysqli('localhost', 'root','', 'bdtrabalho'); 
+$conexao = new mysqli('localhost', 'root','', 'bdtrabalho'); 
 //Variavel = new mysqli("servidor", "usuario", "senha", "banco") or die("Erro ao conectar");
 	if (!$conexao)
 	{ 
