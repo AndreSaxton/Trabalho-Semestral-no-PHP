935f1d412f0b341a9d10fd860d2d053517fb970d
AndreSaxton <andre_saxton@hotmail.com>
2017-09-11 16:44:44 -0300

    Update exibirTabelaPessoa.php

--- exibirTabelaPessoa.php
+++ exibirTabelaPessoa.php
@@ -64,7 +64,7 @@
 		$condicaoBusca =  $_GET["condicaoBusca"];
 		$buscarPor =  $_GET["buscarPor"];
 				
-		$consulta = "SELECT * INTO FROM pessoa";
+		$consulta = "SELECT * FROM pessoa";
 		if(($campoBusca!="")||($condicaoBusca!="")||($buscarPor!="")){
 			if($condicaoBusca=="Nome"){//se buscar nome{
 				if($campoBusca =="")//se nao houver nada preenchido
